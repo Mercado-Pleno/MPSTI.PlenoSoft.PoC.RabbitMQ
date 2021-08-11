c654f9e65d80f59bfd06fea1a28148e0b189ccf4
Bruno N Fernandes <bruno@mercadopleno.com.br>
2021-08-11 01:15:50 -0300

    Implementação da execução em BackGround no aspnet

---
+++ Common/FibonacciService.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Common
+{
+	public class FibonacciService
+	{
+		private static readonly Fibonacci _fibonacci = new Fibonacci();
+		public IEnumerable<long> Gerar(int count)
+		{
+			return _fibonacci.Get(count);
+		}
+
+		public int Count()
+		{
+			return _fibonacci.Count();
+		}
+
+		public bool IsRunning => _fibonacci.IsRunning;
+	}
+
+	public class Fibonacci
+	{
+		private readonly object _access = new object();
+		private readonly List<long> _sequence;
+		private long N1 { get; set; }
+		private long N2 { get; set; }
+		public bool IsRunning { get; private set; }
+
+		public Fibonacci()
+		{
+			N1 = 0;
+			N2 = 1;
+			_sequence = new List<long> { N1, N2 };
+		}
+
+		public int Count() => _sequence.Count;
+
+		public IEnumerable<long> Get(int count)
+		{
+			Generate(count);
+			return _sequence.Take(count);
+		}
+
+		private void Generate(int count)
+		{
+			if (_sequence.Count <= count)
+			{
+				lock (_access)
+				{
+					IsRunning = true;
+					while (_sequence.Count <= count)
+					{
+						var next = Next();
+						_sequence.Add(next);
+						Thread.Sleep(500);
+						//Thread.Sleep(Convert.ToInt32(Math.Max(Math.Min(next, Int32.MaxValue), 0)));
+					}
+					IsRunning = false;
+				}
+			}
+		}
+
+		private long Next()
+		{
+			var n3 = N1 + N2;
+			N1 = N2;
+			return N2 = n3;
+		}
+
+		public static IEnumerable<long> GetSequence(int count)
+		{
+			var fibonacci = new Fibonacci();
+			return fibonacci.Get(count);
+		}
+	}
+}
---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting.Internal;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult GerarRelatorioFibonacci(int id = 20)
+		{
+			var fibonacciService = new FibonacciService();
+			return View("GerarRelatorioFibonacci", fibonacciService.Gerar(id));
+		}
+
+		public IActionResult GerarRelatorioFibonacciBackGround(int id = 20)
+		{
+			var fibonacciService = new FibonacciService();
+			
+			if (!fibonacciService.IsRunning)
+				Task.Factory.StartNew(() => fibonacciService.Gerar(id));
+
+			if (fibonacciService.Count() < id)
+				return View("GerarRelatorioFibonacciBackGround", $"{fibonacciService.Count() * 100 / id} %");
+			else
+				return View("GerarRelatorioFibonacci", fibonacciService.Gerar(id));
+		}
+
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
